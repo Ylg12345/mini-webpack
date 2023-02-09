3b517e3bc2d8785401b7e7c7e9ab4af0dc1f11da
Ylg <v-leiyan@handshakes.com.sg>
2023-02-09 17:11:07 +0800

    changed generate html and bundle method

--- examples/plugins/html-webpack-plugin.js
+++ examples/plugins/html-webpack-plugin.js
@@ -2,34 +2,27 @@ import ejs from 'ejs';
 import path, { dirname } from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+import { generateHtml } from '../../lib/generateBundle.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default class HtmlWebpackPlugin {
   constructor(options) {
     const { filename, title } = options || {};
     this._filename = filename || 'index.html';
     this._title = title;
-    
   }
 
   apply(compiler) {
     compiler.hooks.emit.tapAsync('HtmlWebpackPlugin', (compilation, callback) => {
-
-      const __dirname = dirname(fileURLToPath(import.meta.url));
-
-      const template = fs.readFileSync(  
-        path.resolve(__dirname, './htmlTemplate.ejs'), {
-        encoding: 'utf-8',
-      });
-
+      const templatePath = path.resolve(__dirname, './htmlTemplate.ejs');
       const outputPath = path.join(compilation._output.path, this._filename);
-      const code = ejs.render(template, { 
-        data: {
-          title: this._title,
-          outputFilename: compilation._output.filename,
-        }
-    });
-      fs.writeFileSync(outputPath, code);
+      const htmlOptions = {
+        title: this._title,
+        outputFilename: compilation._output.filename,
+      };
 
+      generateHtml({ htmlOptions, outputPath, templatePath })
       callback();
     })
   }
